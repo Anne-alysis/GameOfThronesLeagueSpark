a3e215046cbd9da0378d37eab5f15de4c338b5b0
Anne Sallaska <annes@porch.com>
2019-04-21 00:19:51 -0700

    adding fake run shell script for reference

---
+++ game-of-thrones-spark/etc/run-github.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+bucket=SCRUBBED
+path=SCRUBBED
+jarname=SCRUBBED
+jar=SCRUBBED
+localpath=SCRUBBED
+
+week=1
+createanswerflag=false
+
+cluster=SCRUBBED
+class=com.github.annealysis.gameofthrones.Score
+
+# clean up old jars
+set +e; gsutil -m rm -rf ${path}/{jarname}; set -e; sleep 1
+
+# push our new jar
+gsutil -m cp ${localpath}/target/${jarname} ${jar}
+sleep 1
+
+gcloud dataproc jobs submit spark \
+    --cluster=${cluster} \
+    --region=us-central1 \
+    --bucket=${bucket} \
+    --jars=${jar} \
+    --class=${class} \
+    -- ${path} \
+    ${week} \
+    ${createanswerflag}
+
